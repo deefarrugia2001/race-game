ec51fe5f1ac3044a317ae4cceff95df78fbd296b
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-05 19:17:52 +0200

    Defined fields pertaining to obstacles for WaveConfig.

--- RacingGame/Assets/Scripts/WaveConfig.cs
+++ RacingGame/Assets/Scripts/WaveConfig.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "WaveConfig")]
 public class WaveConfig : ScriptableObject
 {
+    [SerializeField] GameObject obstacle;
+    [SerializeField] GameObject pathPrefab;
+    [SerializeField] float timeBetweenSpawns;
+    [SerializeField] byte quantity;
+    [SerializeField] float moveSpeed;
 }
