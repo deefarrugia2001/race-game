3fcce276ba32a8ac457c703f7e9ea20b3545de34
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-05 18:57:59 +0200

    A WaveConfig can now be created via Assets context menu.

--- RacingGame/Assets/Scripts/WaveConfig.cs
+++ RacingGame/Assets/Scripts/WaveConfig.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "WaveConfig")]
 public class WaveConfig : ScriptableObject
 {
     void Start()
