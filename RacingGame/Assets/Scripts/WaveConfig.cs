35cbc9b5c5a8a151c092c5779c57d4fff9f44908
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-05 18:57:04 +0200

    WaveConfig now derives from ScriptableObject instead of MonoBehaviour.

--- RacingGame/Assets/Scripts/WaveConfig.cs
+++ RacingGame/Assets/Scripts/WaveConfig.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveConfig : MonoBehaviour
+public class WaveConfig : ScriptableObject
 {
     void Start()
     {
