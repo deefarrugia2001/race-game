883511838a7a45bc22aeed40a6c4d492cc301ecd
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-06 15:20:01 +0200

    Implemented enemy shooting.

--- RacingGame/Assets/Scripts/Enemy.cs
+++ RacingGame/Assets/Scripts/Enemy.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject bulletPrefab;
     [SerializeField] bool canShoot;
+    [SerializeField] float minimumShootTime = .2f, maximumShootTime = 3f;
+    [SerializeField] float projectileSpeed = 20f;
+    float shotCounter;
 
     void Start()
     {
-        
+        shotCounter = Random.Range(minimumShootTime, maximumShootTime);
     }
 
     void Update()
@@ -19,5 +23,18 @@ public class Enemy : MonoBehaviour
 
     void CountDownAndShoot() 
     {
+        shotCounter -= Time.deltaTime;
+        
+        if(shotCounter <= 0) 
+        {
+            Fire();
+            shotCounter = Random.Range(minimumShootTime, maximumShootTime);
+        }
+    }
+
+    void Fire() 
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 }
