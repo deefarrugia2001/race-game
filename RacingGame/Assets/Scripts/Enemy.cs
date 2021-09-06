431c579dd85a3fd09e08a9bc0d252f8ad6654fb5
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-06 15:14:02 +0200

    CountDownAndShoot will be only execute if canShoot is set to true.

--- RacingGame/Assets/Scripts/Enemy.cs
+++ RacingGame/Assets/Scripts/Enemy.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] bool canShoot;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (canShoot)
+            CountDownAndShoot();
+    }
+
+    void CountDownAndShoot() 
+    {
     }
 }
