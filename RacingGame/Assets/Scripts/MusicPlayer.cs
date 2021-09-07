f3339b7c193ec2ed95271f5bf00dd3099db6d0cd
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-07 11:21:55 +0200

    Background music plays flawlessly.
    AudioSource component is added at runtime.

--- RacingGame/Assets/Scripts/MusicPlayer.cs
+++ RacingGame/Assets/Scripts/MusicPlayer.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour
 {
-    static List<>
+    static List<MusicPlayer> musicPlayerInstances;
     [SerializeField] AudioClip backgroundMusic;
     AudioSource audioSource;
-    
-    void Start()
+
+    void Awake()
     {
-        
+        ImplementSingleton();
     }
 
-    void Update()
+    void ImplementSingleton() 
     {
-        
+        musicPlayerInstances = FindObjectsOfType<MusicPlayer>().ToList();
+        if (musicPlayerInstances.Count > 1)
+            Destroy(gameObject);
+        else
+            DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        if(audioSource == null)
+            audioSource = gameObject.AddComponent<AudioSource>();
+
+        audioSource.loop = true;
+        audioSource.playOnAwake = false;
+        audioSource.clip = backgroundMusic;
+        audioSource.Play();
     }
 }
