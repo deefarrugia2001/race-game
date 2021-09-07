4e65f07dbdd3af8866dc2141b61cf4f81f4c8da6
Mandy Farrugia <87620808+deefarrugia2001@users.noreply.github.com>
2021-09-07 09:47:37 +0200

    Implemented Level.

--- RacingGame/Assets/Scripts/Level.cs
+++ RacingGame/Assets/Scripts/Level.cs
@@ -6,4 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class Level : MonoBehaviour
 {
+    [SerializeField] float waitDelay = 2f;
+    GameSession gameSession;
+
+    void Start()
+    {
+        gameSession = FindObjectOfType<GameSession>();    
+    }
+
+    public void LoadMainMenu() 
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame() 
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadWin() 
+    {
+        SceneManager.LoadScene("Win");
+    }
+
+    public void LoadLose() 
+    {
+        StartCoroutine(LoadAfterDelay("Lose"));
+    }
+
+    IEnumerator LoadAfterDelay(string scene) 
+    {
+        yield return new WaitForSeconds(waitDelay);
+        SceneManager.LoadScene(scene);
+    }
 }
